995517622b152c49dad246babf24787b7e3fe6fa
alex <lxalonso@gmail.com>
2023-03-28 18:16:52 +0200

    finalizado pendiente de build

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/vetnmas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./ordenar.component.css']
 })
 export class OrdenarComponent {
+  enMayusuculas: boolean = true;
+  ordenarPor= 'nombre';
+
+  heroes: Heroe[]=[
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Dardevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
+  cambiarEnMayusculas(): void {
+    this.enMayusuculas = !this.enMayusuculas;
+  }
+
+  cambiarOrden ( valor: string): void{
+    console.log(valor)
+    this.ordenarPor= valor;
+  }
 
 }
